2c2e72c15b644ac6074a87566385f9b52b4be437
Mico <mico@micic.ch>
2022-01-03 17:43:26 +0100

    Test pre commit hook

--- test/test_washing_machine.py
+++ test/test_washing_machine.py
@@ -8,7 +8,7 @@ from .util import get_test_response_from_file
 
 
 # Disable retry wait time for better test performance
-BasicDevice.make_vzug_device_call_json.retry.wait = wait_none()  # type: ignore
+BasicDevice.make_vzug_device_call_json.retry.wait = wait_none()
 
 
 async def device_all_information_handler(request: web.BaseRequest):
