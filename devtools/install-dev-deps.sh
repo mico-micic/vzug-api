ecf5a0389d0df65e0e26a05d6fb97ca9855baf1a
Mico <mico@micic.ch>
2022-01-19 21:38:06 +0100

    Fixed dev dependencies problem with multidict and added coverage module

--- devtools/install-dev-deps.sh
+++ devtools/install-dev-deps.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Required because of aiohttp dependency to multidict version <5.0,>=4.5.0
+pip uninstall -y multidict
+pip install multidict==4.7.6
+
 pip install -r $SCRIPT_DIR/dev_requirements.txt
 
 pre-commit install
